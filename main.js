214424852a118ebe2113d949d4f3a803dd668b95
codboz123 <106533407+codboz123@users.noreply.github.com>
2024-04-16 10:57:14 -0300

    .

--- main.js
+++ main.js
@@ -12,8 +12,13 @@ for (let i = 0; i < botoes.length; i++) {
   }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-01");
-let tempoAtual = new Date();
-
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempoObjetivo1 = new Date("2025-03-21T00:00:00");
+function calculaTempo(tempoObjetivo1){
+  let tempoAtual = new Date();
+  contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+  let segundos = Math.floor(tempofinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+}
 
