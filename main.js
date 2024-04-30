805ce86236ab07c75686a1929f0e0778ddf858f3
codboz123 <106533407+codboz123@users.noreply.github.com>
2024-04-30 10:44:41 -0300

    .

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ const tempos = [
 for (let i=0; i< contadores.length; i++){
   contadores[i].textContent = calculaTempo(tempos[i]);
 }
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo1){
   let tempoAtual = new Date();
